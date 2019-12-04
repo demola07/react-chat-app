ec3d9a25753d746983ea6feafe0e8efcae6cf803
demola07 <45898468+demola07@users.noreply.github.com>
2019-12-04 14:44:11 +0100

    created signup component with material ui

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,9 +1,139 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Paper from "@material-ui/core/Paper";
+import withStyles from "@material-ui/core/styles/withStyles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import styles from "./styles";
+// const firebase = require("firebase");
 
 export class SignupComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: null,
+      password: null,
+      passwordConfirmation: null,
+      signupError: ""
+    };
+  }
+
+  formIsvalid = () => {
+    return this.state.password === this.state.passwordConfirmation;
+  };
+
+  userTyping = (type, e) => {
+    switch (type) {
+      case "email":
+        this.setState({ email: e.target.value });
+        break;
+
+      case "password":
+        this.setState({ password: e.target.value });
+        break;
+
+      case "passwordConfirmation":
+        this.setState({ passwordConfirmation: e.target.value });
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  submitSignup = e => {
+    e.preventDefault();
+    // console.log("Submitting", this.state);
+    if (!this.formIsvalid()) {
+      this.setState({ signupError: "Passwords do not match" });
+    }
+  };
+
   render() {
-    return <div>Hello Signup</div>;
+    const { classes } = this.props;
+
+    return (
+      <main className={classes.main}>
+        <CssBaseline></CssBaseline>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Signup!
+          </Typography>
+
+          <form onSubmit={e => this.submitSignup(e)} className={classes.form}>
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="signup-email-input">
+                Enter Your Email
+              </InputLabel>
+              <Input
+                autoComplete="email"
+                onChange={e => this.userTyping("email", e)}
+                autoFocus
+                id="signup-email-input"
+              ></Input>
+            </FormControl>
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="signup-password-input">
+                {" "}
+                Create a Password
+              </InputLabel>
+              <Input
+                type="password"
+                onChange={e => this.userTyping("password", e)}
+                id="signup-password-input"
+              ></Input>
+            </FormControl>
+
+            <FormControl required fullWidth margin="normal">
+              <InputLabel htmlFor="signup-password-confirmation-input">
+                Confirm your Password
+              </InputLabel>
+              <Input
+                type="password"
+                onChange={e => this.userTyping("passwordConfirmation", e)}
+                id="signup-password-confirmation-input"
+              ></Input>
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Submit
+            </Button>
+          </form>
+
+          {this.state.signupError ? (
+            <Typography
+              className={classes.errorText}
+              component="h5"
+              variant="h6"
+            >
+              {this.state.signupError}
+            </Typography>
+          ) : null}
+
+          <Typography
+            component="h5"
+            variant="h6"
+            className={classes.hasAccountHeader}
+          >
+            Already have an Account?
+          </Typography>
+          <Link className={classes.logInLink} to="/login">
+            Log In!
+          </Link>
+        </Paper>
+      </main>
+    );
   }
 }
 
-export default SignupComponent;
+export default withStyles(styles)(SignupComponent);
